6e777381e4fd86a34e2a6de13730c501d68781ae
rebekahfigueroa <figueroarebekah@gmail.com>
2023-06-01 15:07:37 -0500

    added routes for household members

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -6,4 +6,31 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  # household member routes 
+  get "/household_members" do 
+    household_members = Household_member.all.order(:created_at)
+    household_members.to_json
+  end 
+
+  post "/household_members" do 
+    household_member = Household_member.create(
+      name: params[:name], 
+      income: params[:income])
+    household_member.to_json
+  end 
+
+  patch "/household_members/:id" do 
+    household_member = Household_member.find(params[:id])
+    household_member.updates(
+      name: params[:name], 
+      income: params[:income])
+    household_member.to_json
+  end 
+
+  post "/household_members/:id" do 
+    household_member = Household_member.find(params[:id])
+    household_member.destroy
+    household_member.to_json
+  end 
+
 end
