e138be987e6e31efa7dadb5e7a841a0514603b17
rebekahfigueroa <figueroarebekah@gmail.com>
2023-06-05 23:38:07 -0500

    updated CRUD activities in controller for all tables

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -6,6 +6,37 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+# household routes 
+get "/households" do 
+  household = Household.all.order(:created_at)
+  household.to_json
+end 
+
+get "/households/:id" do 
+  household = Household.find(params[:id])
+  household.to_json
+end 
+
+post "/household" do 
+  household = Household.create(
+    name: params[:name])
+  household.to_json
+end 
+
+patch "/household/:id" do 
+  household = Household.find(params[:id])
+  household.updates(
+    name: params[:name])
+  household.to_json
+end 
+
+delete "/household/:id" do 
+  household = Household.find(params[:id])
+  household.destroy
+  household.to_json
+end 
+
+
   # household member routes 
   get "/household_members" do 
     household_members = Household_member.all.order(:created_at)
@@ -19,6 +50,7 @@ class ApplicationController < Sinatra::Base
 
   post "/household_members" do 
     household_member = Household_member.create(
+      household_id: params[:household_id],
       name: params[:name], 
       income: params[:income])
     household_member.to_json
@@ -27,6 +59,7 @@ class ApplicationController < Sinatra::Base
   patch "/household_members/:id" do 
     household_member = Household_member.find(params[:id])
     household_member.updates(
+      household_id: params[:household_id],
       name: params[:name], 
       income: params[:income])
     household_member.to_json
@@ -38,15 +71,56 @@ class ApplicationController < Sinatra::Base
     household_member.to_json
   end 
 
+  # budget routes 
+  get "/budgets" do 
+    budgets = Budgets.all.order(:created_at)
+    budgets.to_json
+  end 
+
+  get "/budgets/:id" do 
+    budget = Budget.find(params[:id])
+    budget.to_json
+  end 
+
+  post "/budgets" do 
+    budget = Budget.create(
+      household_id: params[:household_id], 
+      amount: params[:amount],
+      name: params[:name])
+      budget.to_json
+  end 
+
+  patch "/budgets/:id" do 
+    budget = Budget.find(params[:id])
+    budget.updates(
+      household_id: params[:household_id], 
+      amount: params[:amount],
+      name: params[:name])
+      budget.to_json
+  end 
+
+  delete "/budgets/:id" do 
+    budget = Budget.find(params[:id])
+    budget.destroy
+    budget.to_json
+  end 
+
+
   # budget_event routes 
   get "/budget_events" do 
     budget_events = Budget_event.all.order(:created_at)
     budget_events.to_json
   end 
 
+  get "/budget_events/:id" do 
+    budget_event = Budget_event.find(params[:id])
+    budget_event.to_json
+  end 
+
   post "/budget_events" do 
     budget_event = Budget_event.create(
       household_member_id: params[:household_member_id], 
+      budget_id: params[:budget_id], 
       expense_type: params[:expense_type],
       purchase_type: params[:purchase_type],
       amount: params[:amount],
